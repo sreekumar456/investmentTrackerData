a3f15d34df2ff89ff66c9fbef6a95f967a00e987
sree <sreekumar456@gmail.com>
2020-12-29 01:31:38 +0800

    Adding initial repo files

---
+++ src/main/java/com/it/investmentTracker/controller/AssetOverviewController.java
@@ -0,0 +1,17 @@
+package com.it.investmentTracker.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/asset")
+public class AssetOverviewController {
+
+
+    @RequestMapping(method =  RequestMethod.GET, path = "/data/getAllAssets")
+    public ResponseEntity<String> testClass(){
+        return ResponseEntity.ok("Success");
+    }
+}
