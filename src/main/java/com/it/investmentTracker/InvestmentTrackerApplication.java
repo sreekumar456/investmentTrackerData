4c0b3cc4780b9d2e2e0e071c661f77666fa275ae
sree <sreekumar456@gmail.com>
2020-12-29 02:11:17 +0800

    Adding the package class

--- src/main/java/com/it/investmentTracker/InvestmentTrackerApplication.java
+++ src/main/java/com/it/investmentTracker/InvestmentTrackerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class InvestmentTrackerApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(InvestmentTrackerApplication.class, args);
 	}
 
