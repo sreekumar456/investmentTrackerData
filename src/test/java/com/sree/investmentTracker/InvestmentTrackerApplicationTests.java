539da79dfe892f85d8dd6886ea4e0f386aa31b5f
sree <sreekumar456@gmail.com>
2020-12-28 13:06:09 +0800

    initial commit

---
+++ src/test/java/com/sree/investmentTracker/InvestmentTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sree.investmentTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestmentTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
